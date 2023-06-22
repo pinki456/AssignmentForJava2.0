b47bb96acd49e76009a1c87c8c06d6120f457d29
Pinky Gupta <127490511+pinki456@users.noreply.github.com>
2023-06-22 15:55:13 +0530

    Add files via upload

---
+++ RemoveElement.java
@@ -0,0 +1,24 @@
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int k = 0; // Number of elements not equal to val
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+        int k = removeElement(nums, val);
+        System.out.println(k);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+}
